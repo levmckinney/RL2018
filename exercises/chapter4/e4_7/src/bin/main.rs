7ded4e02c2f260067ff57c773521f3d16aa6a925
levmckinney <levmckinney@gmail.com>
2020-05-03 20:44:43 +0000

    e4_7 created enviroment trait object and came very close to replicating results from the text book

--- exercises/chapter4/e4_7/src/bin/main.rs
+++ exercises/chapter4/e4_7/src/bin/main.rs
@@ -1,11 +1,13 @@
 use e4_7::*;
-
+// Useful for benching
 fn main() {
-  car_env().policy_iteration(
+  let car_env = SimpleCarEnv::new();
+  Environment::policy_iteration(
+    &car_env,
     20.0, // theta
     0.4, // alpha
     1e-4, // epsilon
     Some(0), //start action
     Some(1) //cut off after 10 iterations
-  ).is_some();
+  ).unwrap();
 }
--- exercises/chapter4/e4_7/src/lib.rs
+++ exercises/chapter4/e4_7/src/lib.rs
@@ -1,36 +1,38 @@
-#[macro_use] extern crate cached;
 use rayon::prelude::*;
+use dashmap::DashMap;
 use std::vec::Vec;
-use std::collections::HashMap;
 use std::hash::Hash;
 use std::cmp::Eq;
 use std::cmp::min;
 
-pub struct Environment <State, Action>
-    where State: Hash+Eq+Copy+Send+Sync, Action: Hash+Eq+Copy+Send+Sync {
-    pub states: Vec<State>, // State set
-    pub rewards: Vec<i32>, // Reward set
-    pub actions: fn(&State) -> Vec<Action>, // Action set
+pub trait Environment <State, Action>: Send + Sync
+    where State: Hash+Eq+Copy+Send+Sync, 
+    Action: Hash+Eq+Copy+Send+Sync {
+    fn states(&self) -> &[State]; // State set
+    fn rewards(&self) -> &[i32]; // Reward set
+    fn actions(&self, state: &State) -> Vec<Action>; // Action set
     //dynamics it is assumed that the action will be valid given the current state
-    pub dynamics: fn(&State, &i32, &State, &Action) -> f64,
-    pub gamma: f64, // Discount factor in [0, 1]
+    fn dynamics(&self, next_s: &State, r: &i32, s: &State, a: &Action) -> f64;
+    fn gamma(&self) -> f64; // Discount factor in [0, 1]
 }
 
-impl <State, Action> Environment<State, Action> 
-    where State: Hash+Eq+Copy+Send+Sync, Action: Hash+Eq+Copy+Send+Sync {
+impl <State, Action> dyn Environment<State, Action> 
+    where State: Hash+Eq+Copy+Send+Sync, 
+    Action: Hash+Eq+Copy+Send+Sync{    
     // returns the value of the left hand side of the bellman equation for value function
     pub fn bellman_value_update(
         &self,
         s:&State, 
         a:&Action, 
-        value: &HashMap<State, f64>,
+        value: &DashMap<State, f64>,
         ) -> f64{
-        let Environment{states, rewards, actions:_, dynamics, gamma } = self;
         // sweet parallelism
-        states.par_iter().map(
+        self.states().par_iter().map(
             |next_s| -> f64 {
-                rewards.iter().map(
-                    |r| dynamics(next_s, r, s, a) * ((*r as f64) + (gamma)*value[next_s])
+                self.rewards().iter().map(
+                    |r| {
+                        self.dynamics(next_s, r, s, a) * ((*r as f64) + (self.gamma())*(*value.get(next_s).unwrap()))
+                    }
                 ).sum()
             }
         ).sum()
@@ -38,37 +40,36 @@ impl <State, Action> Environment<State, Action>
 
     pub fn policy_iteration (
         &self,
-        // These hyper parameters are explained above
+        // These hyper parameters are explained in the notebook
         theta: f64,
         alpha: f64,
         epsilon: f64,
         start_action: Option<Action>,
         // n is the number of iterations to stop after.
         // if n is none then the process will run until convergence
-        n:Option<u64>) -> Option<(HashMap<State, Action>, HashMap<State, f64>)> {
-        let Environment{states, rewards:_, actions, dynamics:_, gamma:_ } = self;
-        let mut value: HashMap<State, f64> = HashMap::new(); 
-        let mut policy: HashMap<State, Action> = HashMap::new();
+        n:Option<u64>) -> Option<(DashMap<State, Action>, DashMap<State, f64>)> {
+        let value: DashMap<State, f64> = DashMap::new(); 
+        let policy: DashMap<State, Action> = DashMap::new();
 
         // used to determine convergence/when to stop
         let mut policy_stable: bool;
         let mut value_stable: bool;
-        let mut last_value: HashMap<State, f64>;
-        let mut d_value: HashMap<State, f64> = HashMap::new(); // exponential recent change in value
+        let mut last_value: DashMap<State, f64>;
+        let d_value: DashMap<State, f64> = DashMap::new(); // exponential recent change in value
         let mut i = 0;
 
         // Initialize
-        for s in states {
+        for s in self.states() {
             value.insert(*s, 0.0);
             d_value.insert(*s, 0.0);
             if let Some(action) = start_action{
                 policy.insert(*s, action);
             } else {
-                if actions(s).is_empty() {
+                if self.actions(s).is_empty() {
                     println!("action set empty!");
                     return None;
                 }
-                policy.insert(*s, actions(s)[0]);
+                policy.insert(*s, self.actions(s)[0]);
             }
         }
 
@@ -80,20 +81,21 @@ impl <State, Action> Environment<State, Action>
             println!("Starting evaluation");
             loop {
                 let mut delta = f64::NEG_INFINITY;
-                for s in states {
-                    let v = value[s];
+                for s in self.states() {
+                    let v = *value.get(s).unwrap();
                     value.insert(
                         *s, 
                         self.bellman_value_update(
                             &s,
-                            &policy[s],
-                            &value
+                            &policy.get(s).unwrap(),
+                            &value,
                         )
                     );
-                    delta = if delta > (v - value[s]).abs() {
+                    let change = (v - *value.get(s).unwrap()).abs();
+                    delta = if delta >  change{
                         delta
                     } else {
-                        (v - value[s]).abs()
+                        change
                     }
                 }
                 if delta < theta {
@@ -105,16 +107,16 @@ impl <State, Action> Environment<State, Action>
             // policy improvement
             policy_stable = true;
             println!("Starting improvement");
-            for (i, s) in states.iter().enumerate() {
-                if i % 30 == 0 {println!("-({}/{})", i+1, states.len())}
-                let old_action = policy[s];
+            for (i, s) in self.states().iter().enumerate() {
+                if i % 30 == 0 {println!("-({}/{})", i+1, self.states().len())}
+                let old_action = *policy.get(s).unwrap();
                 let mut greedy_action = old_action;
                 let mut best_value = f64::NEG_INFINITY;
-                for a in actions(s) {
+                for a in self.actions(s) {
                     let value = self.bellman_value_update(
                         &s,
                         &a,
-                        &value
+                        &value,
                     );
                     if value > best_value {
                         best_value = value;
@@ -132,10 +134,15 @@ impl <State, Action> Environment<State, Action>
                 println!("Policy stabilized after {} iterations.", i);
                 break;
             }
+
             value_stable = true;
-            for s in states {
-                d_value.insert(*s, d_value[s] + alpha*((value[s] - last_value[s]) - d_value[s]));
-                if d_value[s] > epsilon {
+            for s in self.states() {
+                let d_value_s = *d_value.get(s).unwrap();
+                let value_s = *value.get(s).unwrap();
+                let last_value_s = *last_value.get(s).unwrap();
+                let d_value_s =  d_value_s + alpha*((value_s - last_value_s) - d_value_s);
+                d_value.insert(*s, d_value_s);
+                if d_value_s > epsilon {
                     value_stable = false;
                 }
             }
@@ -173,7 +180,7 @@ fn poisson(n:u32, lamb:u32) -> f64 {
 // Truncated poisson distribution.
 // if X is poisson(lamb) then min{X, c} is truncated poisson(c, lamb)
 fn trunc_poisson(n:u32, lamb:u32, c:u32) -> f64 {
-    if n < c{
+    if n < c {
         poisson(n, lamb)
     } else if n == c {
         let sum: f64 = (0..c).map(|i| { 
@@ -185,57 +192,76 @@ fn trunc_poisson(n:u32, lamb:u32, c:u32) -> f64 {
     }
 }
 
-// Expected value of truncated poisson
-fn e_trunc_poisson(lamb:u32, c:u32) -> f64{
-    let sum: f64 = (0..c).map(|i| { 
-        ((i - c) as f64) * poisson(i, lamb)
-    }).sum();
-    1.0 + sum
+struct CachedPoisson {
+    trunc_poisson_cache: DashMap<(u32,u32,u32), f64>
 }
 
-// if X_1 follows trunc_poisson(lamb_1, c_1) and X_2 follows trunc_poisson(lamb_2, c_2)
-// then Y = X_1 - X_2 follows dif_trunc_poisson(lamb_1, c_1, lamb_2, c_2)  
-fn dif_trunc_poisson(n:i32, lamb_1:u32, c_1:u32, lamb_2:u32, c_2: u32) -> f64{
-    if n >= 0 {
-        let n = n as u32;
-        (n..=min(c_1, c_2 + n)).map(|n_1| {
-            let n_2 = n_1 - n;
-            trunc_poisson(n_1, lamb_1, c_1) * trunc_poisson(n_2, lamb_2, c_2)
-        }).sum()
-    } else {
-        dif_trunc_poisson(-n, lamb_2, c_2, lamb_1, c_1)
+impl CachedPoisson {
+    fn new() -> CachedPoisson{
+        CachedPoisson {
+            trunc_poisson_cache:DashMap::new()
+        }
+    }
+
+    fn trunc_poisson(&self, n:u32, lamb:u32, c:u32) -> f64 {
+        let prob = *self.trunc_poisson_cache
+        .entry((n, lamb, c))
+        .or_insert_with(|| trunc_poisson(n, lamb, c));
+        prob
     }
 }
 
-pub fn car_env() -> Environment<(i32, i32), i32> {
-    Environment {
-        states: {
-            let mut states = Vec::new();
-            for i in 0..=20 {
-                for j in 0..=20 {
-                    states.push((i,j))
+pub struct SimpleCarEnv {
+    states: Vec<(i32, i32)>,
+    cache: CachedPoisson,
+    rewards: Vec<i32>
+}
+
+impl SimpleCarEnv {
+    pub fn new() -> SimpleCarEnv {
+        SimpleCarEnv {
+            states: {
+                let mut states = Vec::new();
+                for i in 0..=20 {
+                    for j in 0..=20 {
+                        states.push((i,j))
+                    }
                 }
-            }
-            states
-        },
-        rewards: (-10..=400).step_by(2).collect(), // Reward set
-        actions: |s| {
-            (-min(s.1, min(5, 20-s.0))..=min(s.0, min(5, 20-s.1))).collect()
-        }, // Action set
-        //dynamics it is assumed that the action will be valid given the current state
-        dynamics: |next_s, r, s, a| {
-        if (r - a*2) % 10 != 0 {
+                states
+            },
+            cache: CachedPoisson::new(),
+            rewards: (-10..=400).step_by(2).collect()
+        }
+    }
+}
+
+impl Environment<(i32, i32), i32> for SimpleCarEnv {
+    fn states(&self) -> &[(i32, i32)]{
+        &self.states
+    }
+
+    fn actions(&self, s: &(i32, i32)) -> Vec<i32> {
+        let vec: Vec<i32> = (-min(s.1, min(5, 20-s.0))..=min(s.0, min(5, 20-s.1))).collect();
+        vec
+    }
+
+    fn rewards(&self) -> &[i32] {
+        &self.rewards
+    }
+
+    fn dynamics(&self, next_s: &(i32, i32), r: &i32, s: &(i32, i32), a: &i32) -> f64 {
+        let reward_from_cars = r + a.abs()*2;
+        if reward_from_cars % 10 != 0 {
             // this is not possible since we should have sold a natural number of cars
             return 0.0;
         }
-        let num_sold = (r - a*2) / 10;
-        // compute the probability
+        let num_sold = reward_from_cars / 10;
         // sum over disjoint events
         let mut prob = 0.0;
         for sold_at_lot_0 in 0..=num_sold {
             // if we assume the number of cars sold at one lot is fixed then
             // we can compute the probability.
-            // since each one of these events are disjoint we can sum there probabilities.
+            // since each one of these events are disjoint we can sum their probabilities.
             let sold_at_lot_1 = num_sold - sold_at_lot_0;
             let parked_lot_0 = next_s.0 + sold_at_lot_0 + a - s.0;
             let parked_lot_1 = next_s.1 + sold_at_lot_1 - a - s.1;
@@ -245,40 +271,39 @@ pub fn car_env() -> Environment<(i32, i32), i32> {
                 || parked_lot_1 < 0 {
                 continue;
             }
-            prob += trunc_poisson(sold_at_lot_0 as u32, 3, (s.0 - a) as u32)
-                    * trunc_poisson(sold_at_lot_1 as u32, 4, (s.1 + a) as u32)
-                    * trunc_poisson(parked_lot_0 as u32, 3, (20 + sold_at_lot_0 - s.0 + a) as u32)
-                    * trunc_poisson(parked_lot_1 as u32, 2, (20 + sold_at_lot_1 - s.1 - a) as u32)
+            prob += self.cache.trunc_poisson(sold_at_lot_0 as u32, 3, (s.0 - a) as u32)
+                    * self.cache.trunc_poisson(sold_at_lot_1 as u32, 4, (s.1 + a) as u32)
+                    * self.cache.trunc_poisson(parked_lot_0 as u32, 3, (20 + sold_at_lot_0 - s.0 + a) as u32)
+                    * self.cache.trunc_poisson(parked_lot_1 as u32, 2, (20 + sold_at_lot_1 - s.1 - a) as u32);
         }
         prob
-        },
-        gamma: 0.9 // Discount factor in [0, 1]
+    }
+
+    fn gamma(&self) -> f64 {
+        0.9
     }
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn it_works() {
-        assert!(car_env().policy_iteration(
-            0.1, // theta
-            0.4, // alpha
-            1e-4, // epsilon
-            Some(0), //start action
-            Some(10) //cut off after 10 iterations
-        ).is_some());
-    }
-    #[test]
     fn sum_to_one() {
-        let Environment{states, rewards, actions:_, dynamics, gamma:_ } = car_env();
-        let prob: f64 = states.par_iter().map(
-            |next_s| -> f64{
-                rewards.iter().map(
-                    |r| dynamics(next_s, r, &(15,15), &5)
-                ).sum()
-            }
-        ).sum();
-        assert_eq!(prob, 1.0);
+        let env = SimpleCarEnv::new();
+        env.states().par_iter().for_each(|s| {
+            env.actions(&s).iter().for_each(|a| {
+                println!("state {:?} and action {:?}", s, a);
+                let prob: f64 = env.states().iter().map(
+                    |next_s| -> f64 {
+                        env.rewards.iter().map(
+                            |r| env.dynamics(next_s, r, &s, &a)
+                        ).sum()
+                    }
+                ).sum();
+                assert!(0.98 < prob && prob  < 1.02, 
+                    "failed for state {:?} and action {:?}", s, a)
+            });
+        });
     }
 }
